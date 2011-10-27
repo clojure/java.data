bed9f8b8f9bd73be5caa1eca171d97e32cd3c6a4
Tom Faulhaber <git_net@infolace.com>
2011-10-27 00:08:20 -0700

    Initial checkin

---
+++ index-0.0.1.clj
@@ -0,0 +1,24 @@
+{:namespaces
+ ({:source-url
+   "https://github.com/clojure/java.data/blob/487db7420ae07ca4b527b38c39064d3b139ce78d/src/main/clojure/clojure/java/data.clj",
+   :wiki-url
+   "http://clojure.github.com/java.data/clojure.java.data-api.html",
+   :name "clojure.java.data",
+   :author "Cosmin Stejerean",
+   :doc
+   "Support for recursively converting Java beans to Clojure and vice versa."}),
+ :vars
+ ({:name "*to-java-object-missing-setter*",
+   :namespace "clojure.java.data",
+   :source-url
+   "https://github.com/clojure/java.data/blob/487db7420ae07ca4b527b38c39064d3b139ce78d/src/main/clojure/clojure/java/data.clj#L16",
+   :dynamic true,
+   :raw-source-url
+   "https://github.com/clojure/java.data/raw/487db7420ae07ca4b527b38c39064d3b139ce78d/src/main/clojure/clojure/java/data.clj",
+   :wiki-url
+   "http://clojure.github.com/java.data//clojure.java.data-api.html#clojure.java.data/*to-java-object-missing-setter*",
+   :doc
+   "Specify the behavior of missing setters in to-java in the\ndefault object case, using one of :ignore, :log, :error",
+   :var-type "var",
+   :line 16,
+   :file "src/main/clojure/clojure/java/data.clj"})}
