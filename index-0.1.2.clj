39b11fb443350103474b315fa0f87e221b8a6762
Tom Faulhaber <git_net@infolace.com>
2016-01-01 18:14:46 -0800

    Autodoc commit for master/1019b5f4

--- index-0.1.2.clj
+++ index-0.1.2.clj
@@ -1,24 +1,24 @@
 {:namespaces
- ({:source-url
-   "https://github.com/clojure/java.data/blob/92937d1eff36c847581ec391deebe5ba6b029fc5/src/main/clojure/clojure/java/data.clj",
+ ({:doc
+   "Support for recursively converting Java beans to Clojure and vice versa.",
+   :author "Cosmin Stejerean",
+   :name "clojure.java.data",
    :wiki-url
    "http://clojure.github.com/java.data/clojure.java.data-api.html",
-   :name "clojure.java.data",
-   :author "Cosmin Stejerean",
-   :doc
-   "Support for recursively converting Java beans to Clojure and vice versa."}),
+   :source-url
+   "https://github.com/clojure/java.data/blob/92937d1eff36c847581ec391deebe5ba6b029fc5/src/main/clojure/clojure/java/data.clj"}),
  :vars
- ({:name "*to-java-object-missing-setter*",
-   :namespace "clojure.java.data",
+ ({:raw-source-url
+   "https://github.com/clojure/java.data/raw/92937d1eff36c847581ec391deebe5ba6b029fc5/src/main/clojure/clojure/java/data.clj",
+   :name "*to-java-object-missing-setter*",
+   :file "src/main/clojure/clojure/java/data.clj",
    :source-url
    "https://github.com/clojure/java.data/blob/92937d1eff36c847581ec391deebe5ba6b029fc5/src/main/clojure/clojure/java/data.clj#L16",
    :dynamic true,
-   :raw-source-url
-   "https://github.com/clojure/java.data/raw/92937d1eff36c847581ec391deebe5ba6b029fc5/src/main/clojure/clojure/java/data.clj",
-   :wiki-url
-   "http://clojure.github.com/java.data//clojure.java.data-api.html#clojure.java.data/*to-java-object-missing-setter*",
+   :line 16,
+   :var-type "var",
    :doc
    "Specify the behavior of missing setters in to-java in the\ndefault object case, using one of :ignore, :log, :error",
-   :var-type "var",
-   :line 16,
-   :file "src/main/clojure/clojure/java/data.clj"})}
+   :namespace "clojure.java.data",
+   :wiki-url
+   "http://clojure.github.com/java.data//clojure.java.data-api.html#clojure.java.data/*to-java-object-missing-setter*"})}
